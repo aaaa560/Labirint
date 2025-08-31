96499be6f28e5212df5af0f54bd60c4631dfb605
Decaptado <fnafita0201@gmail.com>
2025-08-31 01:45:13 -0300

    Não e mais engine e sim um jogo de labbirintos

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+use std::time::{Duration, Instant};
+
 mod hud;
 mod map;
 mod player;
 mod raycast;
 mod utils;
 
-use crate::map::GameMap;
 use crate::player::Player;
+use crate::{hud::draw_hud, map::GameMap};
 use minifb::{Window, WindowOptions};
 
 fn main() {
@@ -15,7 +17,19 @@ fn main() {
     let mut player = Player::new(1.5, 1.5, 0.0);
     let map = GameMap::default();
 
+    let mut fps_counter = 0;
+    let mut fps = 0;
+    let mut last_fps_update = Instant::now();
+    let fps_update_interval = Duration::from_secs(1);
+
     while window.is_open() && !window.is_key_down(minifb::Key::Escape) {
+        fps_counter += 1;
+        if last_fps_update.elapsed() >= fps_update_interval {
+            fps = fps_counter;
+            fps_counter = 0;
+            last_fps_update = Instant::now();
+            println!("FPS: {}", fps);
+        }
         for i in buffer.iter_mut() {
             *i = 0;
         }
@@ -23,6 +37,7 @@ fn main() {
         player.update(&map, &window);
         raycast::render_scene(&player, &map, &mut buffer);
         hud::draw_minimap(&player, &map, &mut buffer);
+        draw_hud(&player, fps, &mut buffer);
         window.update_with_buffer(&buffer, 800, 600).unwrap();
     }
 }
